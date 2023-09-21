6611de59dc83e826955378234ab48dd7f2e26ff2
kai <sea.k.naka@gmail.com>
2023-09-22 06:45:19 +0900

    frontend 2

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="/css/app.css" rel="stylesheet">
+    <title>Komayuh</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <header>
         <div class="w-full h-auto bg-header-bg bg-cover pb-5">
             <div class="container max-w-5xl px-8">
                 <div class="flex justify-between align-items-center">
-                    <img src="{{ asset('storage/komayuh-image.png') }}" alt="KOMAYUH企画" class="w-2/5 max-w-xs bg-white px-3 py-1 -rotate-12 my-20">
+                    <img src="{{ asset('storage/komayuh-image.png') }}" alt="KOMAYUH企画" class="w-2/5 max-w-xs bg-white px-2 pt-2 pb-10 -rotate-12 my-20">
                     <img src="{{ asset('storage/komayuh-logo.png') }}" alt="KOMAYUH企画" class="w-1/2 bg-white bg-opacity-75 p-3 rounded-tl-3xl rounded-br-3xl">
                 </div>
                 <h4 class="text-lg md:text-xl text-center font-medium">
@@ -234,7 +234,7 @@
             <!-- SNS -->
             <section class="py-10">
                 <div class="flex flex-col items-center justify-center mb-10">
-                    <p class="flex flex-col items-center mb-3">
+                    <p class="flex flex-col items-center mb-8">
                         <span class="font-bold text-2xl md:text-4xl">SNS</span>
                     </p>
                     <div class="flex justify-center items-center">
